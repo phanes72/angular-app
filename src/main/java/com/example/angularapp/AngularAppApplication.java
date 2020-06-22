81305e00efc2b83ef7c51c1d39c56e0f229c7401
Patrick <phanes72@gmail.com>
2020-06-22 11:52:32 -0600

    commit

--- src/main/java/com/example/angularapp/AngularAppApplication.java
+++ src/main/java/com/example/angularapp/AngularAppApplication.java
@@ -1,13 +1,31 @@
 package com.example.angularapp;
 
+import com.example.angularapp.domain.User;
+import com.example.angularapp.domain.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class AngularAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AngularAppApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
 	}
 
 }
---
+++ src/main/java/com/example/angularapp/UserController.java
@@ -0,0 +1,30 @@
+package com.example.angularapp;
+
+import com.example.angularapp.domain.User;
+import com.example.angularapp.domain.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    // standard constructors
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
