4491e400fa3161dd727be8fc8ac9aa2d1c68cc02
Patrick <phanes72@gmail.com>
2020-06-22 10:59:34 -0600

    Inital commit

---
+++ src/main/java/com/example/angularapp/AngularAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/angularapp/AngularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
